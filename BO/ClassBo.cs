97d0825540b9cb358938cddee504896d06c213e2
Joydwip <joyd.piw@gmail.com>
2015-04-22 02:21:22 -0700

    Swap Ur Panel

---
+++ BO/ClassBo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace BO
+{
+    public class ClassBo
+    {
+        Point a = new Point(81, 42);
+        Point b = new Point(423, 42);
+        Point c = new Point(81, 257);
+        Point d = new Point(423, 257);
+
+
+        public int getPanel(Point p, Panel p1, Panel p2, Panel p3, Panel p4)
+        {
+            if (p==p1.Location)
+                return 1;
+            if (p==p2.Location)
+                return 2;
+            if (p==p3.Location)
+                return 3;
+            if (p==p4.Location)
+                return 4;
+            else
+                return 0;
+        }
+
+        public void swapPanel(Point p, Panel thatPanel, Panel p1, Panel p2, Panel p3, Panel p4, int pp)
+        {
+            if(pp==1)
+            {
+                thatPanel.Location = p1.Location;
+                p1.Location = p;
+            }
+            if(pp==2)
+            {
+                thatPanel.Location = p2.Location;
+                p2.Location = p;
+            }
+            if (pp == 3)
+            {
+                thatPanel.Location = p3.Location;
+                p3.Location = p;
+            }
+            if (pp == 4)
+            {
+                thatPanel.Location = p4.Location;
+                p4.Location = p;
+            }
+        }
+
+        public void swap(Panel thatPanel, Panel p1, Panel p2, Panel p3, Panel p4, Point pan)
+        {
+            if (thatPanel.Location.X > 0 && thatPanel.Location.Y > 219 && thatPanel.Location.X < 362 && thatPanel.Location.Y < 461)
+            {
+                int p = getPanel(c, p1, p2, p3, p4);
+                swapPanel(pan, thatPanel, p1, p2, p3, p4, p);
+
+            }
+
+            else if (thatPanel.Location.X > 362 && thatPanel.Location.Y > 0 && thatPanel.Location.X < 742 && thatPanel.Location.Y < 219)
+            {
+                int p = getPanel(b, p1, p2, p3, p4);
+                swapPanel(pan, thatPanel, p1, p2, p3, p4, p);
+
+            }
+
+            else if (thatPanel.Location.X > 362 && thatPanel.Location.Y > 219 && thatPanel.Location.X < 742 && thatPanel.Location.Y < 461)
+            {
+                int p = getPanel(d, p1, p2, p3, p4);
+                swapPanel(pan, thatPanel, p1, p2, p3, p4, p);
+            }
+
+            else if (thatPanel.Location.X > 0 && thatPanel.Location.Y > 0 && thatPanel.Location.X < 362 && thatPanel.Location.Y < 219)
+            {
+
+                int p = getPanel(a, p1, p2, p3, p4);
+                swapPanel(pan, thatPanel, p1, p2, p3, p4, p);
+
+            }
+
+            else
+                thatPanel.Location = pan;
+        }
+
+    }
+}
---
+++ UI/PanelSwap.cs
@@ -0,0 +1,145 @@
+﻿using BO;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Panel_Swap : Form
+    {
+        ClassBo bo = new ClassBo();
+        int temp,p;
+        Student[] s = new Student[4];
+        Point a = new Point(81, 42);
+        Point b = new Point(423, 42);
+        Point c = new Point(81, 257);
+        Point d = new Point(423, 257);
+        
+        Point pan1 = new Point(0,1);
+        Point pan2 = new Point(0,2);
+        Point pan3 = new Point(0,3);
+        Point pan4 = new Point(0,4);
+        
+      
+        public Panel_Swap()
+        {
+            InitializeComponent();
+
+        }
+
+        public void setStudent(Student[] sp)
+        {
+            this.s = sp;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            temp = 1;
+            pan1 = panel1.Location;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            temp = 0;
+            bo.swap(panel1, panel1, panel2, panel3, panel4, pan1);
+           
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (temp == 1)
+            {
+                panel1.Location = new Point(Cursor.Position.X - 200, Cursor.Position.Y - 200);
+            }
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            temp = 2;
+            pan2 = panel2.Location;
+        }
+
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            temp = 0;
+            bo.swap(panel2, panel1, panel2, panel3, panel4, pan2);
+          
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (temp == 2)
+            {
+                panel2.Location = new Point(Cursor.Position.X - 200, Cursor.Position.Y - 200);
+            }
+        }
+
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            temp = 3;
+            pan3 = panel3.Location;
+        }
+
+        private void panel3_MouseUp(object sender, MouseEventArgs e)
+        {
+            temp = 0;
+            bo.swap(panel3, panel1, panel2, panel3, panel4, pan3);
+           
+        }
+
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (temp == 3)
+            {
+                panel3.Location = new Point(Cursor.Position.X - 200, Cursor.Position.Y - 200);
+            }
+        }
+
+        private void panel4_MouseDown(object sender, MouseEventArgs e)
+        {
+            temp = 4;
+            pan4 = panel4.Location;
+        }
+
+        private void panel4_MouseUp(object sender, MouseEventArgs e)
+        {
+            temp = 0;
+            bo.swap(panel4, panel1, panel2, panel3, panel4, pan4);
+           
+        }
+
+        private void panel4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (temp == 4)
+            {
+                panel4.Location = new Point(Cursor.Position.X - 200, Cursor.Position.Y - 200);
+            }
+        }
+
+        private void Panel_Swap_Load(object sender, EventArgs e)
+        {
+            label13.Text = s[0].name;
+            label14.Text = s[0].id;
+            label15.Text = s[0].discipline;
+
+            label16.Text = s[1].name;
+            label17.Text = s[1].id;
+            label18.Text = s[1].discipline;
+
+            label19.Text = s[2].name;
+            label20.Text = s[2].id;
+            label21.Text = s[2].discipline;
+
+            label22.Text = s[3].name;
+            label23.Text = s[3].id;
+            label24.Text = s[3].discipline;
+        }
+    }
+}
